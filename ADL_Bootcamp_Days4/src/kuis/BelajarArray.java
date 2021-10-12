a56015e1ac24ad6cd19192953dbf30d8c32caf4c
GF63 <GF63@MSI.mshome.net>
2021-10-12 14:05:18 +0700

    baru

--- ADL_Bootcamp_Days4/src/kuis/BelajarArray.java
+++ ADL_Bootcamp_Days4/src/kuis/BelajarArray.java
@@ -1,9 +1,36 @@
-
+package kuis;
 
 import java.util.Scanner;
-public class BelajarArray {
-    public static void main (String []asus){
-    Scanner scan = new Scanner(System.in);
-  
-    }
-}
+
+	public class BelajarArray {
+
+	        public static void main(String[] args) {
+
+	            String pattern;
+	            int kaliBerapa;
+	            Scanner scanner = new Scanner(System.in);
+	            System.out.print("Masukan pola yang diinginkan : ");
+	            pattern = scanner.nextLine();
+	            System.out.print("Masukan berapa kali berapa : ");
+	            kaliBerapa = scanner.nextInt();
+
+	            for(int i=1; i<=kaliBerapa; i++) {              
+	                System.out.println();
+	                if(i==1 || i==kaliBerapa) {	          
+	                    for(int j=1; j<=kaliBerapa; j++){	                    
+	                        System.out.print(pattern+" ");	                    
+	                    }
+	                } 
+	                else { 	                    
+	                    for(int k=1; k<=kaliBerapa;k++) {                     
+	                        if(k==1 || k == kaliBerapa) { 	                        
+	                            System.out.print(pattern + " ");	                        
+	                        }                    
+	                        else { 	                         
+	                            System.out.print("  ");	                        
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    }
